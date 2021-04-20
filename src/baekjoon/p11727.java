66b49e4becf7290876977440bb93f9e69bd9292b
한해용 <yong7317>
2021-04-20 14:37:51 +0900

    p11727 런타임 에러 (ArrayIndexOutOfBounds) 수정

--- src/baekjoon/p11727.java
+++ src/baekjoon/p11727.java
@@ -8,12 +8,12 @@ public class p11727 {
 
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
-		int[] dp = new int[n + 1];
+		int[] dp = new int[1001];
         dp[1] = 1;
         dp[2] = 3;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
+            dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10_007;
         }
         System.out.println(dp[n]);
         input.close();
