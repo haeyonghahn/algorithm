9f2541a3134d4642867dbecca5708ffb0620b7cb
haeyong Hahn <yong80211@gmail.com>
2021-04-22 22:11:08 +0900

    DP 2차원 배열 이용하여 풀기

---
+++ src/baekjoon/p11057.java
@@ -0,0 +1,33 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class p11057 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int[][] dp = new int[10000][10];
+		int n = input.nextInt();
+		int sum = 0;
+		
+		for(int i=0; i<=9; i++) {
+			dp[1][i] = 1;
+		}
+		for(int i=2; i<=n; i++) {
+			for(int j=0; j<=9; j++) {
+				if(j == 0) {
+					dp[i][j] = dp[i-1][j];
+				}
+				else {					
+					dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 10007; 
+				}
+			}
+		}
+		for(int i=0; i<=9; i++) {
+			sum = (sum + dp[n][i]) % 10007;
+		}
+		System.out.println(sum);
+		input.close();
+	}
+
+}
