341021709e718a84b790cf9da4445babd1ff565c
haeyong Hahn <yong80211@gmail.com>
2021-04-26 20:53:14 +0900

    피보니치 수열 및 자료형

---
+++ src/baekjoon/p2193.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class p2193 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		long[] dp = new long[91];
+		
+		dp[1] = 1;
+		dp[2] = 1;
+		
+		for(int i=3; i<=n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		System.out.println(dp[n]);
+		input.close();
+	}
+
+}
