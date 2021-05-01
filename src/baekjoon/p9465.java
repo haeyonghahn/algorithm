b3209e6c34fe56071e32dd28cefd95ff3aed3e4b
haeyong Hahn <yong80211@gmail.com>
2021-05-02 02:44:48 +0900

    다이나믹 프로그래밍

--- src/baekjoon/p9465.java
+++ src/baekjoon/p9465.java
@@ -7,40 +7,42 @@ public class p9465 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int t = Integer.parseInt(bufferedReader.readLine());
 
-		int t = Integer.parseInt(bufferedReader.readLine()); // 테스트 케이스의 개수 T
+			for(int i = 0; i < t; i++) {
+				int n = Integer.parseInt(bufferedReader.readLine());
+				int[][] stickers = new int[2][n];
+				int[][] dp = new int[2][n];
 
-		for (int i = 0; i < t; i++) {
-			int n = Integer.parseInt(bufferedReader.readLine()); // n
-			int[][] stickers = new int[2][n];
-			int[][] dp = new int[2][n];
+				String[] input = bufferedReader.readLine().split(" ");
+				for(int j = 0; j < n; j++)
+					stickers[0][j] = Integer.parseInt(input[j]);
 
-			String[] input = bufferedReader.readLine().split(" ");
-			for (int j = 0; j < n; j++)
-				stickers[0][j] = Integer.parseInt(input[j]);
+				input = bufferedReader.readLine().split(" ");
+				for(int j = 0; j < n; j++)
+					stickers[1][j] = Integer.parseInt(input[j]);
 
-			input = bufferedReader.readLine().split(" ");
-			for (int j = 0; j < n; j++)
-				stickers[1][j] = Integer.parseInt(input[j]);
-
-			for (int j = 0; j < n; j++) {
-				if (j == 0) {
-					dp[0][j] = stickers[0][j];
-					dp[1][j] = stickers[1][j];
-				} else if (j == 1) {
-					dp[0][j] = stickers[0][j] + dp[1][j - 1];
-					dp[1][j] = stickers[1][j] + dp[0][j - 1];
-				} else {
-					dp[0][j] = Math.max(
-							Math.max(stickers[0][j] + dp[0][j - 2], stickers[0][j] + dp[1][j - 2]),
-							stickers[0][j] + dp[1][j - 1]);
-					dp[1][j] = Math.max(
-							Math.max(stickers[1][j] + dp[0][j - 2], stickers[1][j] + dp[1][j - 2]),
-							stickers[1][j] + dp[0][j - 1]);
+				for(int j = 0; j < n; j++) {
+					if(j == 0) {
+						dp[0][j] = stickers[0][j];
+						dp[1][j] = stickers[1][j];
+					}
+					else if(j == 1) {
+						dp[0][j] = stickers[0][j] + dp[1][j - 1];
+						dp[1][j] = stickers[1][j] + dp[0][j - 1];
+					}
+					else {
+						dp[0][j] = Math.max(Math.max(stickers[0][j] + dp[0][j - 2], stickers[0][j] + dp[1][j - 2]), stickers[0][j] + dp[1][j - 1]);
+						dp[1][j] = Math.max(Math.max(stickers[1][j] + dp[1][j - 2], stickers[1][j] + dp[0][j - 2]), stickers[1][j] + dp[0][j - 1]);
+					}
 				}
+				System.out.println(Math.max(dp[0][n - 1], dp[1][n - 1]));
 			}
-			System.out.println(Math.max(dp[0][n - 1], dp[1][n - 1]));
+		} catch(Exception e) {
+			throw e;
+		} finally {			
+			bufferedReader.close();
 		}
-		bufferedReader.close();
 	}
 }
