e1a65122eb343b3b100118f3cceed8ffec4959c0
한해용 <yong7317>
2021-05-04 17:20:08 +0900

    다이나믹 프로그래밍 n = 4일 때를 생각해보자

---
+++ src/baekjoon/p2156.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class p2156 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] wine = new int[n+1];
+		int[] dp = new int[n+1];
+		
+		for(int i = 1; i < n+1; i++)
+			wine[i] = scan.nextInt();
+		
+		dp[1] = wine[1]; 
+		if(n >= 2)
+			dp[2] = dp[1] + wine[2];
+		if(n >= 3) {
+			for(int i = 3; i < n+1; i++)
+				dp[i] = Math.max(Math.max(wine[i] + wine[i-1] + dp[i-3], wine[i] + dp[i-2]), dp[i-1]);
+		}
+		System.out.println(dp[n]);
+	}
+}
