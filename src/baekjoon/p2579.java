64c6f20a2d0377bab767b67ca9e46cbe9e70c6b8
haeyong Hahn <yong80211@gmail.com>
2021-05-25 00:49:54 +0900

    다이나믹 프로그래밍. 계단 오르기 (점화식 생각..)

--- src/baekjoon/p2579.java
+++ src/baekjoon/p2579.java
@@ -10,14 +10,20 @@ public class p2579 {
 		int[] stair = new int[n];
 		int[] dp = new int[n];
 		
-		for(int i=0; i<stair.length; i++)
+		for(int i=0; i<n; i++) {
 			stair[i] = input.nextInt();
+		}
 		dp[0] = stair[0];
-		dp[1] = Math.max(stair[1], stair[0] + stair[1]);
-		dp[2] = Math.max(stair[0] + stair[2], stair[1] + stair[2]);
-		
-		for(int i=3; i<n; i++) {
-			dp[i] = Math.max(stair[i] + dp[i - 2], stair[i] + stair[i - 1] + dp[i - 3]);
+		if(n > 1) {			
+			dp[1] = Math.max(stair[1], stair[0] + stair[1]);
+		}
+		if(n > 2) {			
+			dp[2] = Math.max(stair[0] + stair[2], stair[1] + stair[2]);
+		}
+		if(n > 3) {			
+			for(int i=3; i<n; i++) {
+				dp[i] = Math.max(stair[i] + dp[i - 2], stair[i] + stair[i - 1] + dp[i - 3]);
+			}	
 		}
 		System.out.println(dp[n - 1]);
 		input.close();
